a8b4514d645ebd683b77c61fbe2d84b0b0cb0800
Matheus Teixeira <matheustexeirablu@hotmail.com>
2023-02-20 18:23:02 -0300

    PR31 - Redirect para Login quando não Autenticado.

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -5,5 +5,7 @@ import { getToken } from "./services/Getters/lsTokenService"
 export const PrivateRoute = ({ children }) => {
 
   const isAuthenticated = getToken() != null;
-  return isAuthenticated ? children : <Navigate to="/login/signin" />;
+  return (
+    isAuthenticated ? children : <Navigate to="/login/signin" />
+  );
 }
