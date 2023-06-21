5a98c6a1b05ad5a1ccde2af13edac9687a5fb926
Matheus Teixeira <matheustexeirablu@hotmail.com>
2023-06-20 23:07:23 -0300

    a

--- src/themes/colors.js
+++ src/themes/colors.js
@@ -2,6 +2,9 @@ export const COLORS = {
     PrimaryColor: '#0E6BA8',
     SecondColor: '#041A42',
     DetailsColor: '#F2F2F2',
+    Primary: '',
+    Secondary: '',
+    Detail: '',
 
 // #161C23
 // #029DBE
