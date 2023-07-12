a15d5c8c63a68497e5b6b5c653914f07eac5715e
Matheus Teixeira <matheustexeirablu@hotmail.com>
2023-07-12 00:22:47 -0300

    regex para email

--- src/Dispo.Service/DTOs/RequestDTOs/SignInRequestDto.cs
+++ src/Dispo.Service/DTOs/RequestDTOs/SignInRequestDto.cs
@@ -5,8 +5,7 @@ namespace Dispo.Service.DTOs.RequestDTOs
     public class SignInRequestDto
     {
         [Required(ErrorMessage = "O email não pode ser vázio.")]
-        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", 
-                  ErrorMessage = "O email é inválido")]
+        //[RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage = "O email é inválido")]
         public string Email { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "A senha não pode ser vázio.")]
