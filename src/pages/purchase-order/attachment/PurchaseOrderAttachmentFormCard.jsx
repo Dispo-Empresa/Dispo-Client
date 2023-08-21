faff39da4027d725cec4b0219f716c8299c656f5
guilhermelimaaDoAmaral <guilherme_l_amaral@estudante.sc.senai.br>
2023-08-21 13:05:54 -0300

    Up

--- src/pages/purchase-order/attachment/PurchaseOrderAttachmentFormCard.jsx
+++ src/pages/purchase-order/attachment/PurchaseOrderAttachmentFormCard.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import ContentPage from "../../../layouts/content/ContentPage";
-import Datatable from "../../../components/structured/datatable/Datatable";
-import AttachFileIcon from "@mui/icons-material/AttachFile";
-import { ContentOrderAttachmentModal } from "./ContentOrderAttachmentModal";
 import ModalDialog from "../../../components/structured/modal/ModalDialog";
-import getProductsData from "../attachment/Purchase";
+import ButtonGroup from "../../../components/ui/buttons/group/ButtonGroup";
+import { QueryDataButton } from "../../../components/ui/buttons/icons/IconButton"
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import FileUploader from "../attachment/FileUploader";
 
 function PurchaseOrderAttachmentFormCard() {
   const [showModal, setShowModal] = useState(false);
@@ -20,36 +21,64 @@ function PurchaseOrderAttachmentFormCard() {
     console.log("Excluir", rowData);
   };
 
+  const buttonsTemplate = (rowData) => {
+    return (
+      <ButtonGroup>
+        <QueryDataButton
+          onClick={() => {
+            setShowModal(true);
+          }}
+        />
+      </ButtonGroup>
+    );
+  };
+
   const columns = [
     { field: "orderNumber", header: " Número ordem", sort: false, width: 100 },
     { field: "company", header: " Empresa", sort: false, width: 250 },
     { field: "supplier", header: " Fornecedor", sort: false, width: 250 },
-    { field: "creationDate", header: " Data criação", sort: false, width: 100 },
-    { field: "attachment", header: " Anexos", sort: false, width: 100 },
+    { field: "creationDate", header: " Data criação", sort: false, width: 100 }
   ];
 
-  const data = 
+  const data = [
     {
-      orderNumber: 123,
-      company: "benner sistema",
-      supplier: "mercado angelone",
-      creationDate: "12/05/2045"
-    }
+      orderNumber: "PO-001",
+      company: "Empresa A",
+      supplier: "Fornecedor X",
+      creationDate: "2023-08-21", // A data deve estar no formato correto, como "YYYY-MM-DD"
+    },
+    {
+      orderNumber: "PO-002",
+      company: "Empresa B",
+      supplier: "Fornecedor Y",
+      creationDate: "2023-08-22",
+    },
+    {
+      orderNumber: "PO-003",
+      company: "Empresa C",
+      supplier: "Fornecedor Z",
+      creationDate: "2023-08-23",
+    },
+  ];
 
   return (
     <ContentPage title="Anexos de ordem de compra">
-      <ModalDialog title="Modal" open={showModal} onClose={() => setShowModal(false)}>
-        <ContentOrderAttachmentModal />
+      <ModalDialog title="Anexos" open={showModal} onClose={() => setShowModal(false)}>
+       <FileUploader></FileUploader>
       </ModalDialog>
-      <Datatable
-        showCheckbox
-        columns={columns}
-        data={getProductsData}
-        selectedItems={selectedItems}
-        setSelectedItems={setSelectedItems}
-        onViewButton={onViewButton}
-        onDeleteButton={onDeleteButton}
-      />
+      <DataTable value={data} tableStyle={{ minWidth: "50rem" }}>
+        {columns.map((col, i) => (
+          <Column key={col.field} field={col.field} header={col.header} />
+        ))}
+        <Column
+          field="actions"
+          header="Ações"
+          headerStyle={{ minWidth: "180px" }}
+          frozen
+          alignFrozen="right"
+          body={buttonsTemplate}
+        />
+      </DataTable>
     </ContentPage>
   );
 }
