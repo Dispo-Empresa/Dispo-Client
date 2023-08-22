afe315d7eb45ba36fc2e6919dcdaca969f4b8e84
Thiago800 <amaralthiago50@gmail.com>
2023-08-22 19:15:34 -0300

    Adicionando os arquivos na pasta register

---
+++ src/pages/purchase-order/register/StepOne.jsx
@@ -0,0 +1,87 @@
+import { MDBCol } from "mdb-react-ui-kit";
+
+import { UseFieldsStepOne } from "./UseFields";
+import { Step } from "../../../components/structured/stepper/Stepper";
+import { TextField } from "../../../components/ui/inputs/textfield/TextField";
+import { SelectWithFilter } from "../../../components/ui/inputs/select/SelectField";
+
+function StepOne(props){
+    const [fields, handleFieldChange, handleExistsRequiredFieldsNotAnswered] = UseFieldsStepOne();
+
+    const notification = [
+      { value: "Email", label: "Email" },
+      { value: "Whatsapp", label: "Whatsapp" }
+    ];
+
+    const supplier =[
+      {value: "Matheus LTDA", label: "Matheus LTDA"},
+      {value: "Teste MEI", label: "Teste MEI"}
+  ]
+
+    const handleNextStep = () => {
+      if (handleExistsRequiredFieldsNotAnswered()) {
+        props.alertPanel(
+          "warning",
+          "Existem campos obrigatórios não respondidos"
+        );
+        return;
+      }
+
+      props.entityCallback({
+        orderNumber: fields.orderNumber,
+        creationDate: fields.creationDate,
+        notificationType: fields.notificationType,
+        supplier: fields.supplier
+      });
+
+      props.alertPanel(null);
+      props.nextStep();
+    };
+  
+    return (
+      <Step {...props} onNextStep={handleNextStep}>
+        <MDBCol>
+              <TextField
+                type="number"
+                required
+                name="orderNumber"
+                label="Número da ordem"
+                value={fields.orderNumber}
+                onChange={(value) => handleFieldChange("orderNumber", value.target.value)}
+              />
+          </MDBCol>
+          <MDBCol>
+                <SelectWithFilter
+                    required
+                    name="supplier"
+                    label="Fornecedor"
+                    options={supplier}
+                    value={fields.supplier}
+                    onChange={(value) => handleFieldChange("supplier", value.target.value)}
+                />
+          </MDBCol>
+          <MDBCol>
+              <TextField
+                required
+                name="creationDate"
+                label="Data de criação"
+                type="date"
+                value={fields.creationDate}
+                onChange={(value) => handleFieldChange("creationDate", value.target.value)}
+              />    
+          </MDBCol>
+          <MDBCol>
+              <SelectWithFilter
+                required
+                name="notificationType"
+                label="Tipo de notificação"
+                options={notification}
+                value={fields.notificationType}
+                onChange={(value) => handleFieldChange("notificationType", value.target.value)}
+              />    
+          </MDBCol>
+      </Step>
+    );
+  }
+
+  export default StepOne; 
---
+++ src/pages/purchase-order/register/Tiles.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './stylesStepThree.css';
+
+import RegisterPanel from "../../../layouts/panel/register/classic/RegisterPanel";
+
+function Tiles({field, value, isTitle}){
+    return(
+        <RegisterPanel title="Detalhes da ordem de compra" hideSaveButton={true}> 
+        <div className="grayBackground"> 
+            {isTitle ? <h3>{field}: {value}</h3> : <p>{field}: {value}</p>}   
+        </div>    
+      </RegisterPanel>     
+    );
+};
+
+export default Tiles;
